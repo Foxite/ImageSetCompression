ccf5229eaaf78396d6a5a2fdf884b64ef8859ccc
foxite <d.kok.2000@gmail.com>
2020-08-05 16:57:20 +0200

    split activity into fragment, going to add fragments for viewing files and settings. rudimentary fragment-switching code. untested, but compiles.

---
+++ ImageSetCompression.AndroidApp/CompressFragment.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Android.Content;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+
+namespace ImageSetCompression.AndroidApp {
+	public class CompressFragment : Fragment {
+		private const int PickBaseImage = 1;
+		private const int PickSetImages = 2;
+
+		private string m_BaseImagePath;
+		private IEnumerable<string> m_SetImages;
+		private string m_ResultFolder;
+
+		public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			var ret = inflater.Inflate(Resource.Layout.content_compress, container);
+
+			ret.FindViewById<Button>(Resource.Id.compress).Click += (o, e) => AsynchronousOperation("Compressing images...", () => {
+				File.Copy(m_BaseImagePath, Path.Combine(m_ResultFolder, Path.GetFileName(m_BaseImagePath)));
+				ImageSetCompressor.CompressSet(m_BaseImagePath, m_SetImages.Select(item => item), m_ResultFolder);
+			});
+
+			ret.FindViewById<Button>(Resource.Id.decompress).Click += (o, e) => AsynchronousOperation("Decompresing images...", () => {
+				File.Copy(m_BaseImagePath, Path.Combine(m_ResultFolder, Path.GetFileName(m_BaseImagePath)));
+				ImageSetCompressor.DecompressImageSet(m_BaseImagePath, m_SetImages.Select(item => item), m_ResultFolder);
+			});
+
+			ret.FindViewById<Button>(Resource.Id.selectBaseImage).Click += (o, e) => OnSelectBaseImage();
+			ret.FindViewById<Button>(Resource.Id.selectSetImages).Click += (o, e) => OnSelectSetImages();
+
+			return ret;
+		}
+
+		private void AsynchronousOperation(string label, Action operation) {
+			Task.Run(() => {
+				int notificationID = Guid.NewGuid().GetHashCode();
+				var manager = Activity.ApplicationContext.GetSystemService<Android.App.NotificationManager>();
+
+				try {
+					var notifBuilder = new NotificationCompat.Builder(Activity.ApplicationContext, "ImageSetCompressor")
+						.SetSmallIcon(Resource.Mipmap.ic_launcher)
+						.SetContentTitle("ImageSetCompressor")
+						.SetContentText(label)
+						.SetProgress(0, 0, true);
+
+					manager.Notify(notificationID, notifBuilder.Build());
+
+					operation();
+
+					manager.Notify(
+						notificationID,
+						new NotificationCompat.Builder(Activity.ApplicationContext, "ImageSetCompressor")
+							.SetSmallIcon(Resource.Mipmap.ic_launcher)
+							.SetContentTitle("ImageSetCompressor")
+							.SetOngoing(false)
+							.SetContentText("Operation completed")
+							.Build()
+					);
+				} catch (Exception e) {
+					manager.Notify(
+						notificationID,
+						new NotificationCompat.Builder(Activity.ApplicationContext, "ImageSetCompressor")
+							.SetSmallIcon(Resource.Mipmap.ic_launcher)
+							.SetOngoing(false)
+							.SetContentTitle("ImageSetCompressor")
+							.SetContentText("Operation failed")
+							.SetSubText(e.ToStringDemystified())
+							.Build()
+					);
+					throw;
+				}
+			});
+		}
+
+		private void OnSelectBaseImage() {
+			var chooseFile = new Intent(Intent.ActionGetContent);
+			chooseFile.AddCategory(Intent.CategoryOpenable);
+			chooseFile.SetType("image/*");
+			var intent = Intent.CreateChooser(chooseFile, "Select base image");
+			StartActivityForResult(intent, PickBaseImage);
+		}
+		
+		private void OnSelectSetImages() {
+			var chooseFile = new Intent(Intent.ActionGetContent);
+			chooseFile.AddCategory(Intent.CategoryOpenable);
+			chooseFile.PutExtra(Intent.ExtraAllowMultiple, true);
+			chooseFile.SetType("image/*");
+			var intent = Intent.CreateChooser(chooseFile, "Select variant/delta images");
+			StartActivityForResult(intent, PickSetImages);
+		}
+		
+		public override void OnActivityResult(int requestCode, int resultCode, Intent data) {
+			if (resultCode == (int) Android.App.Result.Ok) {
+				if (requestCode == PickBaseImage) {
+					m_BaseImagePath = data.Data.Path;
+					m_ResultFolder = Path.Combine(Path.GetDirectoryName(data.Data.Path), "result");
+					if (!Directory.Exists(m_ResultFolder)) {
+						Directory.CreateDirectory(m_ResultFolder);
+					}
+				} else if (requestCode == PickSetImages) {
+					m_SetImages = new[] { data.Data.Path }; //data.ClipData.AsList();
+				}
+			}
+		}
+
+	}
+}
--- ImageSetCompression.AndroidApp/MainActivity.cs
+++ ImageSetCompression.AndroidApp/MainActivity.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Android;
-using Android.App;
-using Android.Content;
+﻿using Android;
 using Android.OS;
 using Android.Runtime;
 using Android.Support.Design.Widget;
@@ -14,25 +6,19 @@ using Android.Support.V4.App;
 using Android.Support.V4.View;
 using Android.Support.V4.Widget;
 using Android.Support.V7.App;
+using Android.Support.V7.Widget;
 using Android.Views;
-using Android.Widget;
 using ActionBarDrawerToggle = Android.Support.V7.App.ActionBarDrawerToggle;
 
 namespace ImageSetCompression.AndroidApp {
-	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+	[Android.App.Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
 	public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener {
-		private const int PickBaseImage = 1;
-		private const int PickSetImages = 2;
-
-		private string m_BaseImagePath;
-		private IEnumerable<string> m_SetImages;
-		private string m_ResultFolder;
-
 		protected override void OnCreate(Bundle savedInstanceState) {
 			base.OnCreate(savedInstanceState);
 			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
 			SetContentView(Resource.Layout.activity_main);
-			Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+			
+			Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
 			SetSupportActionBar(toolbar);
 
 			DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
@@ -42,92 +28,7 @@ namespace ImageSetCompression.AndroidApp {
 
 			FindViewById<NavigationView>(Resource.Id.nav_view).SetNavigationItemSelectedListener(this);
 
-			FindViewById<Button>(Resource.Id.compress).Click += (o, e) => AsynchronousOperation("Compressing images...", () => {
-				File.Copy(m_BaseImagePath, Path.Combine(m_ResultFolder, Path.GetFileName(m_BaseImagePath)));
-				ImageSetCompressor.CompressSet(m_BaseImagePath, m_SetImages.Select(item => item), m_ResultFolder);
-			});
-
-			FindViewById<Button>(Resource.Id.decompress).Click += (o, e) => AsynchronousOperation("Decompresing images...", () => {
-				File.Copy(m_BaseImagePath, Path.Combine(m_ResultFolder, Path.GetFileName(m_BaseImagePath)));
-				ImageSetCompressor.DecompressImageSet(m_BaseImagePath, m_SetImages.Select(item => item), m_ResultFolder);
-			});
-
-			FindViewById<Button>(Resource.Id.selectBaseImage).Click += (o, e) => OnSelectBaseImage();
-			FindViewById<Button>(Resource.Id.selectSetImages).Click += (o, e) => OnSelectSetImages();
-
 			RequestPermissions(new[] { Manifest.Permission.ReadExternalStorage, Manifest.Permission.WriteExternalStorage }, 1);
-		}
-
-		private void AsynchronousOperation(string label, Action operation) {
-			Task.Run(() => {
-				int notificationID = Guid.NewGuid().GetHashCode();
-				var manager = this.GetSystemService<NotificationManager>();
-
-				try {
-					var notifBuilder = new NotificationCompat.Builder(ApplicationContext, "ImageSetCompressor")
-						.SetSmallIcon(Resource.Mipmap.ic_launcher)
-						.SetContentTitle("ImageSetCompressor")
-						.SetContentText(label)
-						.SetProgress(0, 0, true);
-
-					manager.Notify(notificationID, notifBuilder.Build());
-
-					operation();
-
-					manager.Notify(
-						notificationID,
-						new NotificationCompat.Builder(ApplicationContext, "ImageSetCompressor")
-							.SetSmallIcon(Resource.Mipmap.ic_launcher)
-							.SetContentTitle("ImageSetCompressor")
-							.SetOngoing(false)
-							.SetContentText("Operation completed")
-							.Build()
-					);
-				} catch (Exception e) {
-					manager.Notify(
-						notificationID,
-						new NotificationCompat.Builder(ApplicationContext, "ImageSetCompressor")
-							.SetSmallIcon(Resource.Mipmap.ic_launcher)
-							.SetOngoing(false)
-							.SetContentTitle("ImageSetCompressor")
-							.SetContentText("Operation failed")
-							.SetSubText(e.ToStringDemystified())
-							.Build()
-					);
-					throw;
-				}
-			});
-		}
-
-		private void OnSelectBaseImage() {
-			var chooseFile = new Intent(Intent.ActionGetContent);
-			chooseFile.AddCategory(Intent.CategoryOpenable);
-			chooseFile.SetType("image/*");
-			var intent = Intent.CreateChooser(chooseFile, "Select base image");
-			StartActivityForResult(intent, PickBaseImage);
-		}
-		
-		private void OnSelectSetImages() {
-			var chooseFile = new Intent(Intent.ActionGetContent);
-			chooseFile.AddCategory(Intent.CategoryOpenable);
-			chooseFile.PutExtra(Intent.ExtraAllowMultiple, true);
-			chooseFile.SetType("image/*");
-			var intent = Intent.CreateChooser(chooseFile, "Select variant/delta images");
-			StartActivityForResult(intent, PickSetImages);
-		}
-
-		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data) {
-			if (resultCode == Result.Ok) {
-				if (requestCode == PickBaseImage) {
-					m_BaseImagePath = data.Data.Path;
-					m_ResultFolder = Path.Combine(Path.GetDirectoryName(data.Data.Path), "result");
-					if (!Directory.Exists(m_ResultFolder)) {
-						Directory.CreateDirectory(m_ResultFolder);
-					}
-				} else if (requestCode == PickSetImages) {
-					m_SetImages = new[] { data.Data.Path }; //data.ClipData.AsList();
-				}
-			}
 		}
 
 		public override void OnBackPressed() {
@@ -142,12 +43,21 @@ namespace ImageSetCompression.AndroidApp {
 		public bool OnNavigationItemSelected(IMenuItem item) {
 			int id = item.ItemId;
 
+			void switchFragment<T>() where T : Fragment, new() {
+				FragmentTransaction tx = SupportFragmentManager.BeginTransaction();
+				if (SupportFragmentManager.Fragments.Count != 0) {
+					tx.Remove(SupportFragmentManager.Fragments[0]);
+				}
+				tx.Add(Resource.Id.fragment_container, new T());
+				tx.Commit();
+			}
+
 			if (id == Resource.Id.nav_compress) {
-				
+				switchFragment<CompressFragment>();
 			} else if (id == Resource.Id.nav_view_files) {
-
+				//switchFragment<ViewFragment>();
 			} else if (id == Resource.Id.nav_settings) {
-
+				//switchFragment<SettingsFragment>();
 			} else if (id == Resource.Id.nav_share) {
 
 			} else if (id == Resource.Id.nav_send) {
